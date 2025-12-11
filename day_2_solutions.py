3465437a46ce04a6b854bf284ead35f265d34454
Grant Bridges <grantbridges92@gmail.com>
2025-12-10 21:09:40 -0600

    Stubbed day 2 solutions file

---
+++ day_2_solutions.py
@@ -0,0 +1,15 @@
+from base_aoc_day_solutions import *
+
+# https://adventofcode.com/2025/day/2
+
+class Day2Solutions(BaseAoCDaySolutions):
+    def __init__(self):
+        BaseAoCDaySolutions.__init__(self, 2)
+
+    def part_1(self):
+        return 0
+
+    def part_2(self):
+        return 0
+    
+Day2Solutions().run_solutions()
