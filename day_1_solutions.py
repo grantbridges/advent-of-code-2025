3e38705c59efd15be5337f371b49492a8121b4c6
Grant Bridges <grantbridges92@gmail.com>
2025-12-10 21:09:05 -0600

    Day 1 solutions (pt 1 and 2)

--- day_1_solutions.py
+++ day_1_solutions.py
@@ -7,9 +7,50 @@ class Day1Solutions(BaseAoCDaySolutions):
         BaseAoCDaySolutions.__init__(self, 1)
 
     def part_1(self):
-        return 0
+        current_pos = 50
+        zeros_count = 0
+        with open(self.input_file, 'r') as f:
+            for line in f.readlines():
+                dir = str(line[0]) # L or R
+                dist = int(line[1:]) # number after direction
+
+                delta = 1 if dir == 'R' else -1
+                for i in range(0, dist):
+                    # handle wrapping
+                    if delta == 1 and current_pos == 99:
+                        current_pos = 0
+                    elif delta == -1 and current_pos == 0:
+                        current_pos = 99
+                    else:
+                        current_pos += delta
+                
+                # track if we ended at 0 after this rotation
+                if current_pos == 0:
+                    zeros_count += 1
+        return zeros_count
 
     def part_2(self):
-        return 0
+        current_pos = 50
+        zeros_count = 0
+        with open(self.input_file, 'r') as f:
+            for line in f.readlines():
+                dir = str(line[0]) # L or R
+                dist = int(line[1:]) # number after direction
+
+                delta = 1 if dir == 'R' else -1
+                for i in range(0, dist):
+                    # handle wrapping
+                    if delta == 1 and current_pos == 99:
+                        current_pos = 0
+                    elif delta == -1 and current_pos == 0:
+                        current_pos = 99
+                    else:
+                        current_pos += delta
+                
+                    # track if we ended at 0 after this incremental rotation
+                    if current_pos == 0:
+                        zeros_count += 1
+
+        return zeros_count
     
 Day1Solutions().run_solutions()
