a561cfaed243a4e7ca5200042ad6a6cc1b469e46
Grant Bridges <grantbridges92@gmail.com>
2025-12-14 14:47:36 -0600

    Day 4 stub

---
+++ day_4_solutions.py
@@ -0,0 +1,15 @@
+from base_aoc_day_solutions import *
+
+# https://adventofcode.com/2025/day/4
+
+class Day4Solutions(BaseAoCDaySolutions):
+    def __init__(self):
+        BaseAoCDaySolutions.__init__(self, 4)
+
+    def part_1(self):
+        return 0
+
+    def part_2(self):
+        return 0
+    
+Day4Solutions().run_solutions()
