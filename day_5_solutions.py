8d809aa842137435bd93acc638c59c33c497fcaf
Grant Bridges <grantbridges92@gmail.com>
2025-12-15 14:21:21 -0600

    Day 5 solution stub

---
+++ day_5_solutions.py
@@ -0,0 +1,15 @@
+from base_aoc_day_solutions import *
+
+# https://adventofcode.com/2025/day/5
+
+class Day5Solutions(BaseAoCDaySolutions):
+    def __init__(self):
+        BaseAoCDaySolutions.__init__(self, 5)
+
+    def part_1(self):
+        return 0
+
+    def part_2(self):
+        return 0
+    
+Day5Solutions().run_solutions()
