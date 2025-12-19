e9a60de4b76ee6fbc3ec558ee01ea89f821e3710
Grant Bridges <grantbridges92@gmail.com>
2025-12-18 21:49:40 -0600

    Day 5 pt 1 cleanup

--- day_5_solutions.py
+++ day_5_solutions.py
@@ -10,47 +10,48 @@ class IdRange():
         self.first_id = first_id
         self.last_id = last_id
 
-    def contains(self, id):
+    def contains(self, id: int):
         return self.first_id <= id and self.last_id >= id
+    
+class Util():
+    @staticmethod
+    def read_id_ranges_from_file(input_file) -> List[IdRange]:
+        ranges: List[IdRange] = []
+        with open(input_file, 'r') as f:
+            for line in f.readlines():
+                line = line.strip()
+                if '-' in line:
+                    ids = line.split('-')
+                    ranges.append(IdRange(int(ids[0]), int(ids[1])))
+        return ranges
+    
+    def read_available_ids_from_file(input_file) -> List[int]:
+        ids: List[int] = []
+        with open(input_file, 'r') as f:
+            for line in f.readlines():
+                line = line.strip()
+                if line != '' and '-' not in line:
+                    ids.append(int(line))
+        return ids
 
 class Day5Solutions(BaseAoCDaySolutions):
     def __init__(self):
         BaseAoCDaySolutions.__init__(self, 5)
 
     def part_1(self):
-        fresh_ranges: List[IdRange] = []
-        available_ids = []
-        with open(self.input_file, 'r') as f:
-            reading_ranges = True
-            for line in f.readlines():
-                line = line.strip()
-                if reading_ranges and line == '':
-                    # transition to reading available ingredient Ids
-                    reading_ranges = False
-                    continue
-
-                if reading_ranges:
-                    ids = line.split('-')
-                    fresh_ranges.append(IdRange(int(ids[0]), int(ids[1])))
-                else:
-                    available_ids.append(int(line))
+        fresh_ranges: List[IdRange] = Util.read_id_ranges_from_file(self.input_file)
+        available_ids = Util.read_available_ids_from_file(self.input_file)
 
         log_debug(f'Finished parsing {len(fresh_ranges)} ranges of fresh ingredients')
 
         fresh_avail_count = 0
         for id in available_ids:
-            # found = False
             for fresh_range in fresh_ranges:
                 if fresh_range.contains(id):
                     fresh_avail_count += 1
-                    # found = True
-                    # log_debug(f'Id {id} found in range {fresh_range.first_id}-{fresh_range.last_id}')
                     break
-            # if not found:
-                # log_debug(f'Id {id} was not found in any given range')
 
         return fresh_avail_count
-
 
     def part_2(self):
         return 0
