07aee14422715fea42e23cd040d700c7d199bea1
Grant Bridges <grantbridges92@gmail.com>
2025-12-18 22:36:14 -0600

    Day 5 pt 2 solutions

--- day_5_solutions.py
+++ day_5_solutions.py
@@ -13,6 +13,14 @@ class IdRange():
     def contains(self, id: int):
         return self.first_id <= id and self.last_id >= id
     
+    def overlaps(self, other: IdRange) -> bool:
+        return not (self.last_id < other.first_id or self.first_id > other.last_id)
+    
+    def merge(self, other: IdRange) -> IdRange:
+        # (only call this if you've already checked that they overlap)
+        self.first_id = min(self.first_id, other.first_id)
+        self.last_id = max(self.last_id, other.last_id)
+    
 class Util():
     @staticmethod
     def read_id_ranges_from_file(input_file) -> List[IdRange]:
@@ -25,6 +33,7 @@ class Util():
                     ranges.append(IdRange(int(ids[0]), int(ids[1])))
         return ranges
     
+    @staticmethod
     def read_available_ids_from_file(input_file) -> List[int]:
         ids: List[int] = []
         with open(input_file, 'r') as f:
@@ -34,6 +43,38 @@ class Util():
                     ids.append(int(line))
         return ids
 
+    # Returns a copy of the provided list of IdRanges that's been sorted
+    # and all overlapping ranges have been merged together
+    @staticmethod
+    def merge_ranges(ranges: List[IdRange]):
+        # sort ranges on first id
+        ranges = sorted(ranges, key=lambda r: r.first_id)
+
+        pos = 0
+        a = ranges[pos]
+        b = ranges[pos+1]
+
+        while True:
+            while a.overlaps(b):
+                a.merge(b)
+                ranges.remove(b) # delete b after merging into a
+
+                if pos + 1 < len(ranges):
+                    # grab new next entry to check against a again
+                    b = ranges[pos+1]
+                else:
+                    break # at end of comparisons
+            
+            if pos + 2 < len(ranges):
+                pos += 1
+                a = ranges[pos]
+                b = ranges[pos+1]
+            else:
+                break # nothing left to compare
+
+        return ranges
+
+
 class Day5Solutions(BaseAoCDaySolutions):
     def __init__(self):
         BaseAoCDaySolutions.__init__(self, 5)
@@ -41,8 +82,6 @@ class Day5Solutions(BaseAoCDaySolutions):
     def part_1(self):
         fresh_ranges: List[IdRange] = Util.read_id_ranges_from_file(self.input_file)
         available_ids = Util.read_available_ids_from_file(self.input_file)
-
-        log_debug(f'Finished parsing {len(fresh_ranges)} ranges of fresh ingredients')
 
         fresh_avail_count = 0
         for id in available_ids:
@@ -54,6 +93,17 @@ class Day5Solutions(BaseAoCDaySolutions):
         return fresh_avail_count
 
     def part_2(self):
-        return 0
+        ranges: List[IdRange] = Util.read_id_ranges_from_file(self.input_file)
+
+        ranges = Util.merge_ranges(ranges)
+
+        total_ids = 0
+        for r in ranges:
+            first_id = r.first_id
+            last_id = r.last_id
+            num_ids = last_id - first_id + 1 # inclusive
+            total_ids += num_ids
+
+        return total_ids
     
 Day5Solutions().run_solutions()
